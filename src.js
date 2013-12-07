40d07be87a35ff54763285ddc36fcd1cb59d87b4
Shreyas <shreyasrulez@gmail.com>
2013-12-08 00:55:31 +0530

    multiline support added

--- src.js
+++ src.js
@@ -39,16 +39,53 @@ function draw(){
 	ctx.drawImage(vicp, 0,0,500,500)
 	
 	ctx.font = "bold " + fst.toString() + "px arial"
-	ctx.fillText(txtTop, 250, fst)
-	ctx.strokeText(txtTop, 250, fst)
+	//ctx.fillText(txtTop, 250, fst)
+	//ctx.strokeText(txtTop, 250, fst)
 	
-	ctx.font = "bold " + fsb.toString() + "px arial"
-	ctx.fillText(txtBottom, 250, 500-10)
-	ctx.strokeText(txtBottom, 250, 500-10)
+	var drawFunc = function(context, line, x, y){
+		context.fillText(line, x, y)
+		context.strokeText(line, x, y)
+	}
+	
+	var topLines = splitLine(ctx, txtTop, ctx.font, 480)
+	drawTextMultiline(ctx, topLines, 250, fst, drawFunc)
+	
+	var bottomLines = splitLine(ctx, txtBottom, ctx.font, 480)
+	var voff = 500-10
+	if (bottomLines[1]!=""){
+		voff = voff - ctx.measureText("M").width
+	}
+	drawTextMultiline(ctx, bottomLines, 250, voff, drawFunc)
 }
 
+function drawTextMultiline(ctx, lines, x, y, drawfunc){
+	var offset = 0
+	lines.forEach(function(line){
+		drawfunc(ctx, line, x, y+offset)
+		offset = offset + ctx.measureText("M").width
+	})
+}
+
+//font is of format: "[style] [size] [family]"
+function splitLine(ctx, str, font, width){
+	var words = str.split(" ")
+	ctx.font = font
+	var l=1
+	var r=words.length+1
+	while (r-l>1){
+		var m = Math.floor((l+r)/2)
+		if (ctx.measureText(words.slice(0, 0+m).join(" ")).width <= width){
+			l=m
+		} else {
+			r=m
+		}
+	}
+	return new Array(words.slice(0,0+l).join(" "), words.slice(0+l,words.length).join(" "))
+}
+
+//font is only font family
 function fitText(ctx, str, font, style, width){
-	var l=20
+	var l=36
 	var r=100
 	while (r-l>1){
 		var m=Math.floor((l+r)/2)
